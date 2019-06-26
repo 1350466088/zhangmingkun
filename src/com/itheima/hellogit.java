7f645cdeeb027fb5079ad50d8b8dfbd9153b2e68
张三 <zhangsan@itcast>
2019-06-26 19:41:04 +0800

    添加test工程至git

---
+++ src/com/itheima/hellogit.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+    }
+}
